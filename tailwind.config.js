b6452a2d31620579dc3e6104d27a78ac05ffc9ae
un-earthly <vijayalamin@gmail.com>
2022-05-21 10:56:53 -0700

    tailwind daisy ui and few packages installed

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: ["corporate"],
+  },
+  plugins: [require("daisyui")],
+}
