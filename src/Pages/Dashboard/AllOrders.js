cb656d15990d98f43c009f37334b607cdbb04576
un-earthly <vijayalamin@gmail.com>
2022-05-25 11:58:05 -0700

    admin and users features done

--- src/Pages/Dashboard/AllOrders.js
+++ src/Pages/Dashboard/AllOrders.js
@@ -1,16 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Helmet from 'react-helmet'
+import { toast } from 'react-toastify'
 import axiosPrivate from '../../api/axiosPrivate'
 import useOrders from '../../Hooks/useOrders'
 import Loading from '../../SharedComponents/Loading'
 
 export default function AllOrders() {
   const { isLoading, orders, refetch } = useOrders()
+  const [id, setId] = useState('')
   const updateShipped = id => {
+
     axiosPrivate.patch('https://dry-bayou-12932.herokuapp.com/orders', { id })
       .then(res => {
         refetch()
       })
+  }
+  const deleteOrder = () => {
+    axiosPrivate.delete('https://dry-bayou-12932.herokuapp.com/orders/' + id).then(res => {
+      toast.success('Deleted Successfull')
+      refetch()
+    })
   }
   if (isLoading) {
     return <Loading />
@@ -23,6 +32,14 @@ export default function AllOrders() {
       <div>
         <h1 className="text-3xl font-bold p-5">Add Products</h1>
       </div>
+      <input type="checkbox" id="deleteModal" className="modal-toggle" />
+      <div className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <p>Do You Want To Delete?</p>
+          <label htmlFor="deleteModal" onClick={deleteOrder} className="btn btn-success">Yes</label>
+          <label htmlFor="deleteModal" className="btn btn-error" >No</label>
+        </div>
+      </div>
       <div class="overflow-x-auto">
         <table class="table w-full">
           <thead>
@@ -31,9 +48,7 @@ export default function AllOrders() {
               <th>Name</th>
               <th>payment Status</th>
               <th>Shipped Status</th>
-              <th>Ship To</th>
-              <th>User's Phone</th>
-              <th>User's Email</th>
+              <th>Delete</th>
             </tr>
           </thead>
           <tbody>
@@ -43,10 +58,9 @@ export default function AllOrders() {
                   <th>{i + 1}</th>
                   <td>{o.name}</td>
                   <td>{o.paymentStatus ? <span className="text-success">Paid <span className="block">{o.transactionId}</span></span> : <span className="text-error">Not Paid</span>}</td>
-                  <td onClick={() => updateShipped(o._id)}>{o.shippedStatus ? <span className="text-success">Shipped</span> : <span className="text-error">Not Shipped</span>}</td>
-                  <td>{o.address}</td>
-                  <td>{o.phone}</td>
-                  <td>{o.email}</td>
+                  <td><button disabled={!o.paymentStatus} onClick={() => updateShipped(o._id)}>{o.shippedStatus ? <span className="text-success">Shipped</span> : <span className="text-error">Not Shipped</span>}</button></td>
+                  <td>{!o.paymentStatus ? <label className="badge bg-error border-0" htmlFor="deleteModal" onClick={() => setId(o._id)}><i className="bi bi-x "></i></label> : <span className="text-error">Order Cant Be Canceled</span>}</td>
+
                 </tr>
               ))
             }
